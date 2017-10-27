5f6c7ec0123831505acd0116a88e47cfee7f4cf7
kenorb <kenorb@users.noreply.github.com>
2017-10-27 18:15:54 +0100

    Improves Apache install and shell code (GH-4)

--- vagrant/bootstrap.sh
+++ vagrant/bootstrap.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
-
-# Portions of this script taken from https://gist.github.com/rrosiek/8190550
+# @see: https://gist.github.com/rrosiek/8190550
 
 # Variables
 SITEURL=newtvp.example.com
@@ -9,38 +8,33 @@ DBPASSWD=root
 
 export DEBIAN_FRONTEND=noninteractive
 
-# purge, then install and configure apache
-apt-get purge apache2*
-apt-get update
-apt-get install -y -o Dpkg::Options::="--force-confold"
+# Install and configure Apache.
+apt-get install -y -o Dpkg::Options::="--force-confold" apache2
 if ! [ -L /var/www ]; then
   rm -rf /var/www
   ln -fs /vagrant /var/www
 fi
-a2enmod rewrite > /dev/null 2>&1
-a2enmod ssl > /dev/null 2>&1
+a2enmod rewrite
+a2enmod ssl
 
 # install php packages
-apt-get install -y php5 php5-cli libapache2-mod-php5 php5-mcrypt
-apt-get install -y php5-mysql
-apt-get install -y php5-curl
-apt-get install -y php5-xdebug
+apt-get install -y php5 php5-cli libapache2-mod-php5 php5-mcrypt php5-mysql php5-curl php5-xdebug
 sed -i "s/error_reporting = .*/error_reporting = E_ALL/" /etc/php5/apache2/php.ini
 sed -i "s/display_errors = .*/display_errors = On/" /etc/php5/apache2/php.ini
-if grep "xdebug" /etc/php5/apache2/php.ini > /dev/null
+if ! grep "xdebug" /etc/php5/apache2/php.ini > /dev/null
 	then
-	   echo 'do nothing' >> /dev/null
-	else
-		echo '[xdebug]
+	  cat >> /etc/php5/apache2/php.ini <<EOF
+[xdebug]
 xdebug.idekey="debugit"
 xdebug.remote_host=10.0.2.2
 xdebug.remote_port=10000
 xdebug.remote_enable=1
 xdebug.remote_autostart=0
-xdebug.remote_handler="dbgp"' >> /etc/php5/apache2/php.ini
+xdebug.remote_handler="dbgp"
+EOF
 fi 
 
-# install mysql and phpmyadmin
+# Install mysql and phpmyadmin.
 echo "mysql-server mysql-server/root_password password $DBPASSWD" | debconf-set-selections
 echo "mysql-server mysql-server/root_password_again password $DBPASSWD" | debconf-set-selections
 echo "phpmyadmin phpmyadmin/dbconfig-install boolean true" | debconf-set-selections
@@ -48,10 +42,10 @@ echo "phpmyadmin phpmyadmin/app-password-confirm password $DBPASSWD" | debconf-s
 echo "phpmyadmin phpmyadmin/mysql/admin-pass password $DBPASSWD" | debconf-set-selections
 echo "phpmyadmin phpmyadmin/mysql/app-pass password $DBPASSWD" | debconf-set-selections
 echo "phpmyadmin phpmyadmin/reconfigure-webserver multiselect none" | debconf-set-selections
-apt-get -y install mysql-server-5.6 phpmyadmin > /dev/null 2>&1
-a2enconf phpmyadmin > /dev/null 2>&1
+apt-get -y install mysql-server-5.6 phpmyadmin > /dev/null
+a2enconf phpmyadmin > /dev/null
 
-# add virtual host for the site
+# Add virtual host for the site.
 cat > /etc/apache2/sites-enabled/000-default.conf <<EOF
 <VirtualHost *:80>
     DocumentRoot /var/www/newtvp
@@ -87,10 +81,8 @@ cat > /etc/apache2/sites-enabled/000-default.conf <<EOF
 EOF
 
 # Add swap memory - https://www.digitalocean.com/community/tutorials/how-to-add-swap-on-ubuntu-14-04
-if grep "/swapfile" /etc/fstab > /dev/null
+if ! grep "/swapfile" /etc/fstab > /dev/null
 	then
-	   echo 'do nothing' >> /dev/null
-	else
 		fallocate -l 4G /swapfile
 		chmod 600 /swapfile
 		mkswap /swapfile
@@ -102,5 +94,5 @@ fi
 apt-get install -y unzip
 
 # Restart apache and mysql
-service apache2 restart > /dev/null 2>&1
-service mysql restart > /dev/null 2>&1
+service apache2 restart > /dev/null
+service mysql restart > /dev/null
